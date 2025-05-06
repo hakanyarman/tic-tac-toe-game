7ecb3ccf3b54b41943aae16b668fe9c49cace8a6
Hakan Yarman <104122864+hakanyarman@users.noreply.github.com>
2025-05-06 05:18:55 +0300

    Refactor App component and add Square component for tic-tac-toe game layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,53 @@
-function App() {
+// Bir dosyada export default kullanırsan: O dosyayı başka bir yerde import ederken, parantez içine almak zorunda değilsin
+// default olduğunda import kısmında ismi değiştirebilirsin, bu bir zorunluluk değil.
+
+// export (Named Export)
+
+// export function mesajVer() {
+//   console.log("Merhaba Dünya!");
+// }
+
+// export const selam = "Selamlar!";
+
+// import { mesajVer, selam } from './mesaj.js';
+
+// yalnızca en az bir kez manuel olarak yazdığınız kodu kopyalamanızı tavsiye ederiz.
+
+// interfaceler PascalCase yazılır.
+
+import "./styles.css"
+interface SquareProps {
+  value: number
+}
+
+function Square({ value }: SquareProps) {
+  const handleClick = () => {
+    //console.log(`${value} button clicked`)
+  }
+  return <button onClick={handleClick} className="square">{value}</button>
+}
+
+
+export default function Board() {
   return (
     <>
-      the tictactoe game in react documentation
+      {/* div elementinin display özelliği block'tur yani bir sonraki element bir alt satırda başlar */}
+      <div className="board-row">
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
+      </div>
+      <div className="board-row">
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
+      </div>
+      <div className="board-row">
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
+      </div>
     </>
   )
 }
 
-export default App
